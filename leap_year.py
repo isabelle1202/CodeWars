7ed5fcafcd5ad16a4593fa27fb078c39f7c03609
Isabelle Nowak <isabelle.coding.1202@gmail.com>
2023-12-01 17:21:33 +0100

    left out one file in previous commit

--- leap_year.py
+++ leap_year.py
@@ -1,3 +1,11 @@
+# In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+
+# Years divisible by 4 are leap years,
+# but years divisible by 100 are not leap years.
+
+# Years divisible by 400 ARE leap years.
+
+
 def is_leap_year(year):
     """defines if a gives year is a leap year"""
 
