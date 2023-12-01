203b3bf96e74a373de4a5fa04af7481cef3a7e26
Isabelle Nowak <isabelle.coding.1202@gmail.com>
2023-12-01 17:21:18 +0100

    Added instructions to each file.

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,6 @@
 def is_leap_year(year):
     """defines if a gives year is a leap year"""
+
     if year % 400 == 0:
         return True
     if year % 4 == 0 and year % 100 != 0:
--- name_in.py
+++ name_in.py
@@ -1,3 +1,12 @@
+# What's in a name?
+# ...Or rather, what's a name in? For us, a particular string is where we are looking for a name.
+
+# Task
+# Write a function, taking two strings in parameter, that tests whether or not the first string contains all of the letters of the second string, in order.
+
+# The function should return true if that is the case, and else false. Letter comparison should be case-INsensitive.
+
+
 def name_in_str(strng: str, name: str) -> bool:
     name_index = 0
     strng = strng.lower()
