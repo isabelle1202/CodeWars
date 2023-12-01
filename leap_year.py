1209fe46dcb734ef58cac9f87ff0672a28ba206f
Isabelle Nowak <isabelle.coding.1202@gmail.com>
2023-12-01 12:17:50 +0100

    leap_year determination

---
+++ leap_year.py
@@ -0,0 +1,10 @@
+def is_leap_year(year):
+    """defines if a gives year is a leap year"""
+    if year % 400 == 0:
+        return True
+    if year % 4 == 0 and year % 100 != 0:
+        return True
+    if year % 100 == 0:
+        return False
+    else:
+        return False
